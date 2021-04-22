9312103213fe84fc310b114d50e26b2ac2989486
Sean Sundberg <seansund@us.ibm.com>
2021-04-21 23:50:57 -0500

    Updates resource_location value in test
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-hpvs.tf
+++ test/stages/stage2-hpvs.tf
@@ -3,7 +3,7 @@ module "dev_hpvs" {
 
   ibmcloud_api_key    = var.ibmcloud_api_key
   resource_group_name = module.resource_group.name
-  resource_location   = var.region
+  resource_location   = "dal10"
   name_prefix         = var.name_prefix
   plan                = var.plan
   sshPublicKey        = module.vpcssh.public_key
