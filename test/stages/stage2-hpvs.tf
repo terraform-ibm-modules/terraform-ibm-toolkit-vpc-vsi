4229f62f8c5aadc9a5f45905ef12e5b069e7dba1
Sean Sundberg <seansund@us.ibm.com>
2021-04-22 00:18:13 -0500

    Hard code ssh key
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-hpvs.tf
+++ test/stages/stage2-hpvs.tf
@@ -6,5 +6,5 @@ module "dev_hpvs" {
   resource_location   = "dal10"
   name_prefix         = var.name_prefix
   plan                = "entry"
-  sshPublicKey        = module.vpcssh.public_key
+  sshPublicKey        = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzz9XOjhB99/2X2+3wb0hBij+fQpv6FPJZ3A02UHKBW7sLdPnbwHKj7Np9+atFeRWY+V+gtStAveLKWoMrVGab0e+vtYVRzq5HjClDWpqgJZTqwpK2yhQx2bBaT+RTl/fQWpoT4+JENXHupvVi1oMRhyMkKyZT+r6Tb+E85RhfRRnohOcReC+4frZLq8HKTREANM30k3lZs7cGw+tmTgLCRVeR2V+Z1T6LA+tC6JbQq7UrsvLUpwmqc9VN84hVuKtiktSeX7aGkuEib0U6thQYlGrlfZzRNXsXYtW7tyZK2+yd3ctezhKCs25ReAc+Ek7tUA3TZCNcvL5LdXKk72n7 yogendra@yogendras-mbp.c4l-in.ibmmobiledemo.com"
 }
