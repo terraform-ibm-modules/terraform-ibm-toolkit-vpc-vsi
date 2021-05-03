03fe676dc7582c7f6f3d827b1c565ba2cbb7c6c0
Sean Sundberg <seansund@us.ibm.com>
2021-05-03 18:09:36 -0500

    Updates outputs
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,48 @@
-output "id" {
+output "ids" {
   description = "The instance id"
-  value       = data.ibm_resource_instance.hpvs_instance.id
+  value       = ibm_is_instance.vsi[*].id
 }
 
-output "name" {
+output "names" {
   description = "The instance name"
-  value       = local.name
-  depends_on  = [data.ibm_resource_instance.hpvs_instance]
+  value       = ibm_is_instance.vsi[*].name
 }
 
-output "crn" {
+output "crns" {
   description = "The crn of the instance"
-  value       = data.ibm_resource_instance.hpvs_instance.id
+  value       = ibm_is_instance.vsi[*].id
+}
+
+output "public_ips" {
+  description = "The public ips of the instances"
+  value       = var.create_public_ip ? ibm_is_floating_ip.vsi[*].address : []
+}
+
+output "private_ips" {
+  value = ibm_is_instance.vsi.primary_network_interface[*].primary_ipv4_address
+  description = "The private ips of the instances"
 }
 
 output "location" {
   description = "The instance location"
-  value       = var.resource_location
-  depends_on  = [data.ibm_resource_instance.hpvs_instance]
+  value       = var.region
+  depends_on  = [ibm_is_instance.vsi]
 }
 
 output "service" {
-  description = "The name of the key provisioned for the instance"
-  value       = local.service
-  depends_on = [data.ibm_resource_instance.hpvs_instance]
+  description = "The name of the service for the instance"
+  value       = "is"
+  depends_on = [ibm_is_instance.vsi]
+}
+
+output "type" {
+  description = "The type of the service for the instance"
+  value       = "instance"
+  depends_on = [ibm_is_instance.vsi]
 }
 
 output "label" {
   description = "The label used for the instance"
   value       = var.label
-  depends_on = [data.ibm_resource_instance.hpvs_instance]
+  depends_on = [ibm_is_instance.vsi]
 }
