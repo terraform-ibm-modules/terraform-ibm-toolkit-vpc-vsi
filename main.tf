86a3f5b2332e64d04a3312e095200dfb7a14d76d
Sean Sundberg <seansund@us.ibm.com>
2021-05-03 20:21:07 -0500

    Rewrites module (#1)
    * Rewrites module to create a general virtual server instance
    
    Co-authored-by: Yogendra Srivastava <ysrivastava@in.ibm.com>

--- main.tf
+++ main.tf
@@ -1,44 +1,89 @@
 
+locals {
+  name        = "${replace(var.vpc_name, "/[^a-zA-Z0-9_\\-\\.]/", "")}-${var.label}"
+  tags        = tolist(setunion(var.tags, [var.label]))
+}
+
 resource null_resource print_names {
   provisioner "local-exec" {
-    command = "echo 'Resource group: ${var.resource_group_name}'"
+    command = "echo 'Resource group: ${var.resource_group_id}'"
+  }
+  provisioner "local-exec" {
+    command = "echo 'VPC name: ${var.vpc_name}'"
   }
 }
 
-data "ibm_resource_group" "resource_group" {
+data "ibm_is_image" "image" {
+  name = var.image_name
+}
+
+data ibm_is_vpc vpc {
   depends_on = [null_resource.print_names]
 
-  name = var.resource_group_name
+  name  = var.vpc_name
 }
 
-locals {
-  name_prefix = var.name_prefix != "" ? var.name_prefix : var.resource_group_name
-  name        = var.name != "" ? var.name : "${replace(local.name_prefix, "/[^a-zA-Z0-9_\\-\\.]/", "")}-hpcs"  
+resource ibm_is_security_group vsi {
+  name           = "${local.name}-group"
+  vpc            = data.ibm_is_vpc.vpc.id
+  resource_group = var.resource_group_id
 }
 
-resource "ibm_resource_instance" "hpvs_instance" {
-  name              = local.name
-  service           = "hpvs"
-  plan              = var.plan
-  location          = var.resource_location
-  resource_group_id = data.ibm_resource_group.resource_group.id
-  tags              = var.tags
+resource ibm_is_security_group_rule ssh_inbound {
+  count = var.allow_ssh_from != "" ? 1 : 0
 
-  parameters = {
-      sshPublicKey = var.sshPublicKey
- } 
-  timeouts {
-    create = "15m"
-    update = "15m"
-    delete = "15m"
+  group     = ibm_is_security_group.vsi.id
+  direction = "inbound"
+  remote    = var.allow_ssh_from
+  tcp {
+    port_min = 22
+    port_max = 22
   }
 }
 
-data "ibm_resource_instance" "hpvs_instance" {
-  depends_on        = [ibm_resource_instance.hpvs_instance]
+resource ibm_is_instance vsi {
+  count = var.vpc_subnet_count
 
-  name              = local.name
-  resource_group_id = data.ibm_resource_group.resource_group.id
-  location          = var.resource_location
-  service           = "hpvs"
+  name           = "${local.name}${format("%02s", count.index)}"
+  vpc            = data.ibm_is_vpc.vpc.id
+  zone           = var.vpc_subnets[count.index].zone
+  profile        = var.profile_name
+  image          = data.ibm_is_image.image.id
+  keys           = var.ssh_key_ids
+  resource_group = var.resource_group_id
+
+  user_data = var.init_script != "" ? var.init_script : file("${path.module}/scripts/init-script-ubuntu.sh")
+
+  primary_network_interface {
+    subnet          = var.vpc_subnets[count.index].id
+    security_groups = [ibm_is_security_group.vsi.id]
+  }
+
+  boot_volume {
+    name = "${local.name}${format("%02s", count.index)}-boot"
+  }
+
+  tags = var.tags
+}
+
+resource ibm_is_floating_ip vsi {
+  count = var.create_public_ip ? var.vpc_subnet_count : 0
+
+  name           = "${local.name}${format("%02s", count.index)}-ip"
+  target         = ibm_is_instance.vsi[count.index].primary_network_interface[0].id
+  resource_group = var.resource_group_id
+
+  tags = var.tags
+}
+
+resource ibm_is_security_group_rule ssh_to_self_public_ip {
+  count = var.create_public_ip ? var.vpc_subnet_count : 0
+
+  group     = ibm_is_security_group.vsi.id
+  direction = "outbound"
+  remote    = ibm_is_floating_ip.vsi[count.index].address
+  tcp {
+    port_min = 22
+    port_max = 22
+  }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,48 @@
-#output "myoutput" {
-#  description = "Description of my output"
-#  value       = "value"
-#  depends_on  = [<some resource>]
-#}
+output "ids" {
+  description = "The instance id"
+  value       = ibm_is_instance.vsi[*].id
+}
+
+output "names" {
+  description = "The instance name"
+  value       = ibm_is_instance.vsi[*].name
+}
+
+output "crns" {
+  description = "The crn of the instance"
+  value       = ibm_is_instance.vsi[*].id
+}
+
+output "public_ips" {
+  description = "The public ips of the instances"
+  value       = var.create_public_ip ? ibm_is_floating_ip.vsi[*].address : []
+}
+
+output "private_ips" {
+  value = ibm_is_instance.vsi[*].primary_network_interface[0].primary_ipv4_address
+  description = "The private ips of the instances"
+}
+
+output "location" {
+  description = "The instance location"
+  value       = var.region
+  depends_on  = [ibm_is_instance.vsi]
+}
+
+output "service" {
+  description = "The name of the service for the instance"
+  value       = "is"
+  depends_on = [ibm_is_instance.vsi]
+}
+
+output "type" {
+  description = "The type of the service for the instance"
+  value       = "instance"
+  depends_on = [ibm_is_instance.vsi]
+}
+
+output "label" {
+  description = "The label used for the instance"
+  value       = var.label
+  depends_on = [ibm_is_instance.vsi]
+}
