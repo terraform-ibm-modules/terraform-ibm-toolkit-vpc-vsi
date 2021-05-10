cb0bbd9a15fa3b948960687daa13068407511c58
Sean Sundberg <seansund@us.ibm.com>
2021-05-10 18:13:59 -0500

    Collapses security group rule creation into a single resource (#15)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -44,58 +44,39 @@ resource ibm_is_security_group_rule ssh_inbound {
   }
 }
 
-resource ibm_is_security_group_rule additional_all_rules {
-  for_each = {
-  for rule in var.security_group_rules : rule.name => rule if lookup(rule, "tcp", null) == null && lookup(rule, "udp", null) == null && lookup(rule, "icmp", null) == null
-  }
+resource ibm_is_security_group_rule additional_rules {
+  count = length(var.security_group_rules)
+
   group      = ibm_is_security_group.vsi.id
-  direction  = each.value.direction
-  remote     = each.value.remote
-  ip_version = lookup(each.value, "ip_version", null)
-}
+  direction  = var.security_group_rules[count.index]["direction"]
+  remote     = lookup(var.security_group_rules[count.index], "remote", null)
+  ip_version = lookup(var.security_group_rules[count.index], "ip_version", null)
 
-resource ibm_is_security_group_rule additional_tcp_rules {
-  for_each = {
-  for rule in var.security_group_rules : rule.name => rule if lookup(rule, "tcp", null) != null
+  dynamic "tcp" {
+    for_each = lookup(var.security_group_rules[count.index], "tcp", null) != null ? [ lookup(var.security_group_rules[count.index], "tcp", null) ] : []
+
+    content {
+      port_min = tcp.value["port_min"]
+      port_max = tcp.value["port_max"]
+    }
   }
-  group      = ibm_is_security_group.vsi.id
-  direction  = each.value.direction
-  remote     = each.value.remote
-  ip_version = lookup(each.value, "ip_version", null)
 
-  tcp {
-    port_min = each.value.tcp.port_min
-    port_max = each.value.tcp.port_max
+  dynamic "udp" {
+    for_each = lookup(var.security_group_rules[count.index], "udp", null) != null ? [ lookup(var.security_group_rules[count.index], "udp", null) ] : []
+
+    content {
+      port_min = udp.value["port_min"]
+      port_max = udp.value["port_max"]
+    }
   }
-}
 
-resource ibm_is_security_group_rule additional_udp_rules {
-  for_each = {
-  for rule in var.security_group_rules : rule.name => rule if lookup(rule, "udp", null) != null
-  }
-  group      = ibm_is_security_group.vsi.id
-  direction  = each.value.direction
-  remote     = each.value.remote
-  ip_version = lookup(each.value, "ip_version", null)
+  dynamic "icmp" {
+    for_each = lookup(var.security_group_rules[count.index], "icmp", null) != null ? [ lookup(var.security_group_rules[count.index], "icmp", null) ] : []
 
-  udp {
-    port_min = each.value.udp.port_min
-    port_max = each.value.udp.port_max
-  }
-}
-
-resource ibm_is_security_group_rule additional_icmp_rules {
-  for_each = {
-  for rule in var.security_group_rules : rule.name => rule if lookup(rule, "icmp", null) != null
-  }
-  group      = ibm_is_security_group.vsi.id
-  direction  = each.value.direction
-  remote     = each.value.remote
-  ip_version = lookup(each.value, "ip_version", null)
-
-  icmp {
-    type = each.value.icmp.type
-    code = lookup(each.value.icmp, "code", null) == null ? null : each.value.icmp.code
+    content {
+      type = icmp.value["type"]
+      code = lookup(icmp.value, "code", null)
+    }
   }
 }
 
